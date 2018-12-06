5871ca46aad5938138c0d788838c94270419f237
bmoreini <42300498+bmoreini@users.noreply.github.com>
2018-12-06 11:58:00 -0500

    Hide Key

--- rpg-tollbooth.js
+++ rpg-tollbooth.js
@@ -14,7 +14,7 @@ function getScene(record_id) {
   // Replace with your own AirTable API key.
   // Normally, you will want to keep this private.
   // This key will only be good for a couple of days.
-  const key = 'keyRVFBEY8xeNmXzS';
+  const key = 'keyxxxxxxxx';
   // Alter this to match your own AirTable base.
   // URL format is
   // https://api.airtable.com/v0/<BASE_ID>/<TABLE_NAME>/<RECORD_ID>?api_key=<YOUR_API_KEY>
